9ba17ccd9d03c81af1dce973d461ffd3e9bc2bcd
Axel López <115676305+Ajderniz@users.noreply.github.com>
2024-04-15 10:23:40 -0600

    Add exercise 2 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(sString, iTimes) {
 
+	if (iTimes < 0) {
+		return 'ERROR';
+	} //end if
+
+	let sReturnString = '';
+	for (let iIndex = 0; iIndex < iTimes; iIndex++) {
+		sReturnString += sString;
+	} //end for
+	return sReturnString;
 };
 
 // Do not edit below this line
