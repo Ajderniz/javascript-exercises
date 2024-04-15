84a2698dd8a1bedc311f1ec573375eec18a3cd92
Axel López <115676305+Ajderniz@users.noreply.github.com>
2024-04-15 12:19:41 -0600

    Add exercise 5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(iNumber1, iNumber2) {
+
+	if (iNumber1 < 0 || iNumber2 < 0) {
+		return 'ERROR';
+	} //end if
+	if (!Number.isInteger(iNumber1) || !Number.isInteger(iNumber2)) {
+		return 'ERROR';
+	} //end if
+	if (iNumber1 > iNumber2) {
+		//swap
+		const iTemp = iNumber1;
+		iNumber1 = iNumber2;
+		iNumber2 = iTemp;
+	} //end if
+
+	let iSum = 0;
+	for (let iIndex = iNumber1; iIndex <= iNumber2; iIndex++) {
+		iSum += iIndex;
+	} //end for
+
+	return iSum;
 
 };
 
