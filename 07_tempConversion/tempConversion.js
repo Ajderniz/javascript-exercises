5e9d26554fbd9a30b9d5d2a27c95d6968ac82360
Axel López <115676305+Ajderniz@users.noreply.github.com>
2024-04-16 09:49:47 -0600

    Add exercise 7 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundDecimal(fDecimal) {
+	return Math.round(fDecimal * 10) / 10;
+} //end roundDecimal
+
+const convertToCelsius = function(fFahrenheit) {
+	return roundDecimal((fFahrenheit - 32) / 1.8);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fCelsius) {
+	return roundDecimal((fCelsius * 1.8) + 32);
 };
 
 // Do not edit below this line
